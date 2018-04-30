9a93a279e043e4924ef9726fc3bdbe35a4c9499e
Mario Germán Reyes C <mager19@gmail.com>
2018-04-30 15:39:50 -0500

    blog

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -5,26 +5,21 @@
 	$args = array( 'post_type' => 'post' ,'posts_per_page' => 2, 'category__not_in' => 35 );
 	$loop = new WP_Query( $args );
 
-	if ( $loop->have_posts() ) : ?>
-
-		<div class="col-md-6">
-		
-		<?php
-		while ( $loop->have_posts() ) : $loop->the_post(); ?>
-	
+	if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
+	<!-- post -->
+	<div class="col-md-6">
 		<div class="post-entry" style="background: url(<?php the_post_thumbnail_url(); ?>);background-size: cover;">
 			<div class="post-scuare">
 				<p><?php the_date(); ?></p>
-				<h2 class="titulos__blog"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<p class="post-categories"><b> <?php the_category( '' ); ?>					
 				</b></p>
 			</div>
 		</div>
-	
+	</div>
 	<?php endwhile; ?>
 	<!-- post navigation -->
 	<?php else: ?>
 	<!-- no posts found -->
-	</div>
 	<?php endif; ?>
 	<?php wp_reset_postdata();?>
